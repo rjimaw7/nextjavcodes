7db7bbae883d340188d4aca70e77000cc78bfae4
rjimaw7 <rjimaw7777@gmail.com>
2024-02-02 22:17:54 +0800

    added hydration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import HomeComponent from '@/components/HomeComponent';
+import getQueryClient from '@/lib/getQueryClient';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Test Potato</h1>
+      <HydrationBoundary state={dehydrate(getQueryClient())}>
+        <HomeComponent />
+      </HydrationBoundary>
     </main>
   );
 }
---
+++ src/components/HomeComponent.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+const HomeComponent = () => {
+  return <div>Homeqwe</div>;
+};
+
+export default HomeComponent;
