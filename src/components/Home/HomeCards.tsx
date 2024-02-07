917f5af0ed776590fe17a9b553d488b4bc833fd9
rjimaw7 <rjimaw7777@gmail.com>
2024-02-07 19:07:04 +0800

    adjusted mobile cards mobile ui

--- src/components/Home/HomeCards.tsx
+++ src/components/Home/HomeCards.tsx
@@ -55,7 +55,7 @@ const HomeCards = () => {
   }, [inView, hasNextPage]);
 
   return (
-    <section id="cards" className="container flex flex-col gap-6">
+    <section id="cards" className="flex flex-col gap-6 md:container">
       <HomeFilter sort={sort} setSort={setSort} allCodesLoading={allCodesLoading} />
 
       <div className="grid grid-cols-1 gap-6 pb-10 md:grid-cols-2 md:gap-8 lg:mx-20 lg:grid-cols-3">
